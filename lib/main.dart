23416c710d4d45d1597d99fc0c97e27136bd849a
liuyu <liu282207134@yahoo.co.jp>
2024-04-25 09:28:19 +0900

    Merge remote-tracking branch 'origin/master'

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:intl/intl.dart';
-import 'package:flutter_localization/flutter_localization.dart';
 
 void main() {
   runApp(MyApp());
@@ -269,29 +267,8 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
         drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: const <Widget>[
-              UserAccountsDrawerHeader(
-                //用户账户抽屉头
-                accountName: Text("Liu Yu"), //账户名称
-                accountEmail: Text("xxxxxxyahoo.co.jp"), //账户邮箱
-                currentAccountPicture: CircleAvatar(
-                  radius: 50,
-                  //当前帐户图片:圈子头像
-                  backgroundImage: AssetImage('images/panda.png'),
-                ),
-              ),
-              ListTile(
-                title: Text('Gallery'),
-              ),
-              ListTile(
-                leading: Icon(Icons.home),
-                title: Text("Home"),
-              ),
-            ],
-          ),
-        ),
+            // Drawer的内容省略，因为这部分没有问题
+            ),
         bottomNavigationBar: BottomNavigationBar(
           items: [
             BottomNavigationBarItem(
